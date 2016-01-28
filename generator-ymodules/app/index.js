caf94ca36f0bdec6007ad133e34329a724fe75a8
Alexey Sazonov <sazonov@yandex-team.ru>
2016-01-28 10:49:41 +0300

    simple yeoman ymodule generator

---
+++ generator-ymodules/app/index.js
@@ -0,0 +1,59 @@
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+    constructor: function () {
+        generators.Base.apply(this, arguments);
+
+        this.appname = 'ymodule';
+    },
+
+    prompting: function () {
+        var done = this.async(),
+            questions = [
+                {
+                    input: 'input',
+                    name: 'appname',
+                    message: 'Your project name',
+                    default: this.appname
+                },
+                {
+                    input: 'input',
+                    name: 'username',
+                    message: 'Your name',
+                    default: ''
+                }
+            ];
+
+        this.prompt(questions, function (answers) {
+            this.userData = answers;
+            done();
+        }.bind(this));
+    },
+
+    writing: {
+      packageJSON: function () {
+        this.fs.copyTpl(
+          this.templatePath('_package.json'),
+          this.destinationPath('package.json'),
+          {appname: this.userData.appname, username: this.userData.username}
+        );
+      },
+      editorconfig: function () {
+        this.fs.copy(this.templatePath('editorconfig'), this.destinationPath('.editorconfig'));
+      },
+      copy: function () {
+        this.fs.copy(this.templatePath('eslintrc'), this.destinationPath('.eslintrc'));
+        this.fs.copy(this.templatePath('babelrc'), this.destinationPath('.babelrc'));
+        this.fs.copy(this.templatePath('gitignore'), this.destinationPath('.gitignore'));
+        this.fs.copy(this.templatePath('build.json'), this.destinationPath('build.json'));
+      }
+    },
+
+    install: function () {
+      this.installDependencies();
+    },
+
+    end: function () {
+      this.log('Buy!');
+    }
+});
