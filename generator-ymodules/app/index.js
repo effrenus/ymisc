c1ef237a6393d4cc5baa5b559f969d6f752b2f3f
Alexey Sazonov <effrenus@gmail.com>
2016-02-02 17:28:54 +0300

    flow, es6 options

--- generator-ymodules/app/index.js
+++ generator-ymodules/app/index.js
@@ -4,6 +4,18 @@ module.exports = generators.Base.extend({
     constructor: function () {
         generators.Base.apply(this, arguments);
 
+        this.option('flow', {
+          type: Boolean,
+          defaults: false,
+          desc: 'Flow syntax support'
+        });
+
+        this.option('es6', {
+          type: Boolean,
+          defaults: false,
+          desc: 'ES6 syntax support via babel'
+        });
+
         this.appname = 'ymodule';
     },
 
@@ -35,21 +47,53 @@ module.exports = generators.Base.extend({
         this.fs.copyTpl(
           this.templatePath('_package.json'),
           this.destinationPath('package.json'),
-          {appname: this.userData.appname, username: this.userData.username}
+          {
+            appname: this.userData.appname,
+            username: this.userData.username,
+            flow: this.options['flow'],
+            es6: this.options['es6']
+          }
         );
+      },
+      babel: function () {
+        if (!this.options['flow'] && !this.option['es6']) {
+          return;
+        }
+        this.fs.copyTpl(
+          this.templatePath('babelrc'),
+          this.destinationPath('.babelrc'),
+          {
+            flow: this.options['flow'],
+            es6: this.options['es6']
+          }
+        );
+      },
+      build: function () {
+        this.fs.copyTpl(
+          this.templatePath('build.json'),
+          this.destinationPath('build.json'),
+          {
+            flow: this.options['flow'],
+            es6: this.options['es6']
+          }
+        );
+      },
+      flow: function () {
+        if (!this.options['flow']) {
+          return;
+        }
+        this.fs.copy(this.templatePath('flowconfig'), this.destinationPath('.flowconfig'));
       },
       copy: function () {
         this.fs.copy(this.templatePath('editorconfig'), this.destinationPath('.editorconfig'));
         this.fs.copy(this.templatePath('eslintrc'), this.destinationPath('.eslintrc'));
-        this.fs.copy(this.templatePath('babelrc'), this.destinationPath('.babelrc'));
         this.fs.copy(this.templatePath('gitignore'), this.destinationPath('.gitignore'));
-        this.fs.copy(this.templatePath('build.json'), this.destinationPath('build.json'));
         this.fs.copy(this.templatePath('gulpfile.js'), this.destinationPath('gulpfile.json'));
       }
     },
 
     install: function () {
-      this.installDependencies();
+      // this.installDependencies();
     },
 
     end: function () {
